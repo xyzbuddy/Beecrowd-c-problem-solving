5dd4611fecf8cf51aa52aff225f726d0d80c2480
Parvel Al Deen <parvejhossin734@gmail.com>
2025-10-05 02:19:13 +0600

    Added solution for Beecrowd 1016

---
+++ 1016.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int distance;
+    int time;
+
+    scanf("%d", &distance);
+
+    time = distance * 2;
+
+    printf("%d minutos\n", time);
+
+    return 0;
+}
